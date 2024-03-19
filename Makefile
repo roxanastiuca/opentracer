683b0920c3aaf3336295137b84d126d1bd2131e4
Roxana Stiuca <roxana9stiuca@gmail.com>
2024-03-19 14:10:52 +0100

    Initial commit

---
+++ Makefile
@@ -0,0 +1,36 @@
+TARGET = opentracer
+
+BPF_TARGET = ${TARGET:=.bpf}
+BPF_C = ${BPF_TARGET:=.c}
+BPF_OBJ = ${BPF_TARGET:=.o}
+
+USER_C = ${TARGET:=.c}
+USER_SKEL = ${TARGET:=.skel.h}
+
+COMMON_H = ${TARGET:=.h}
+
+all: $(TARGET) $(BPF_OBJ)
+.PHONY: all
+
+$(TARGETS): %: %.bpf.o 
+
+$(TARGET): $(USER_C) $(USER_SKEL) $(COMMON_H)
+	gcc -Wall -o $(TARGET) $(USER_C) -L../libbpf/src -l:libbpf.a -lelf -lz
+
+$(BPF_OBJ): %.o: $(BPF_C) vmlinux.h  $(COMMON_H)
+	clang \
+	    -target bpf \
+		-I/usr/include/$(shell uname -m)-linux-gnu \
+		-g \
+	    -O2 -o $@ -c $<
+	llvm-strip -g $@
+
+$(USER_SKEL): $(BPF_OBJ)
+	bpftool gen skeleton $< > $@
+
+vmlinux.h:
+	bpftool btf dump file /sys/kernel/btf/vmlinux format c > vmlinux.h
+
+clean: 
+	- rm $(BPF_OBJ)
+	- rm $(TARGET)
---
+++ opentracer.c
@@ -0,0 +1,97 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include "opentracer.skel.h"
+#include "opentracer.h"
+
+static volatile bool keep_running = true;
+
+static void sig_handler(int)
+{
+    keep_running = false;
+}
+
+int handle_event(void *ctx, void *data, size_t data_sz)
+{
+	const struct event *e = data;
+	struct tm *tm;
+	char ts[32];
+	time_t t;
+
+	time(&t);
+	tm = localtime(&t);
+	strftime(ts, sizeof(ts), "%H:%M:%S", tm);
+
+	printf("%-8s %-5s %-7d %-16s %s\n", ts, "EXEC", e->pid, e->comm, e->fname);
+
+	return 0;
+}
+
+int main()
+{
+    LIBBPF_OPTS(bpf_object_open_opts, open_opts);
+    struct opentracer_bpf *obj;
+    int err;
+    
+    struct ring_buffer *rb = NULL;
+
+    /* clean handle Ctrl-C */
+    signal(SIGINT, sig_handler);
+    signal(SIGTERM, sig_handler);
+
+    libbpf_set_strict_mode(LIBBPF_STRICT_ALL);
+
+    obj = opentracer_bpf__open_opts(&open_opts);
+    if (!obj) {
+        fprintf(stderr, "failed to open BPF object\n");
+        return 1;
+    }
+
+	// /* aarch64 and riscv64 don't have open syscall */
+    // if (!tracepoint_exists("syscalls", "sys_enter_open")) {
+	// 	bpf_program__set_autoload(obj->progs.tracepoint__syscalls__sys_enter_open, false);
+	// }
+
+    err = opentracer_bpf__load(obj);
+    if (err) {
+        fprintf(stderr, "failed to load BPF object: %d\n", err);
+        goto cleanup;
+    }
+
+    err = opentracer_bpf__attach(obj);
+    if (err) {
+        fprintf(stderr, "failed to attach BPF programs: %d\n", err);
+        goto cleanup;
+    }
+
+    rb = ring_buffer__new(bpf_map__fd(obj->maps.events), handle_event, NULL, NULL);
+    if (!rb) {
+        fprintf(stderr, "failed to create ring buffer\n");
+        goto cleanup;
+    }
+
+    printf("OK\n");
+
+    while (keep_running) {
+        err = ring_buffer__poll(rb, 10000);
+        if (err == -EINTR) {
+			err = 0;
+			break;
+		}
+		if (err < 0) {
+			printf("Error polling ring buffer: %d\n", err);
+			break;
+		}
+    }
+
+cleanup:
+    ring_buffer__free(rb);
+	opentracer_bpf__destroy(obj);
+	// cleanup_core_btf(&open_opts); // TODO: fix
+
+    return err != 0;
+}
